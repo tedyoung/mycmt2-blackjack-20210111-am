be1c74402b90ae17b2da8922df9d920b14947c5c
Ted M. Young <tedyoung@gmail.com>
2021-01-11 08:45:42 -0800

    Lab 1 - Extract and move display (transformation) to ConsoleCard.

--- src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -10,7 +10,7 @@ class CardDisplayTest {
   public void displayTenAsString() throws Exception {
     Card card = new Card(Suit.HEARTS, Rank.TEN);
 
-    assertThat(card.display())
+    assertThat(ConsoleCard.display(card))
         .isEqualTo("\u001B[31m┌─────────┐\u001B[1B\u001B[11D│10       │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♥    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│       10│\u001B[1B\u001B[11D└─────────┘");
   }
 
@@ -18,7 +18,7 @@ class CardDisplayTest {
   public void displayNonTenAsString() throws Exception {
     Card card = new Card(Suit.SPADES, Rank.SEVEN);
 
-    assertThat(card.display())
+    assertThat(ConsoleCard.display(card))
       .isEqualTo("\u001B[30m┌─────────┐\u001B[1B\u001B[11D│7        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♠    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        7│\u001B[1B\u001B[11D└─────────┘");
   }
 }
