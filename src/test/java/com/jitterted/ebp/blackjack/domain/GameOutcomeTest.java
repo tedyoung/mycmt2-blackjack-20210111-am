e3ec960a6e17923caab10705eeb27c712c06f143
Ted M. Young <tedyoung@gmail.com>
2021-01-12 09:33:57 -0800

    Lab 4a: stub deck with player busts test

---
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -0,0 +1,23 @@
+package com.jitterted.ebp.blackjack.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GameOutcomeTest {
+
+  @Test
+  public void playerBustsAndLoses() throws Exception {
+    Deck stubDeck = new StubDeck(Rank.JACK, Rank.TWO,
+                                 Rank.QUEEN, Rank.THREE,
+                                 Rank.SIX);
+    Game game = new Game(stubDeck);
+
+    game.initialDeal();
+    game.playerHits();
+
+    assertThat(game.determineOutcome())
+        .isEqualTo("You Busted, so you lose.  💸");
+  }
+
+}
